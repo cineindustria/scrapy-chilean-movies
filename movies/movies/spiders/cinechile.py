a973e7c0a95a8ef8766320fe4c75e85b251e51d9
Nicolás Silva <sovietfrance@gmail.com>
2016-05-08 00:47:54 -0300

    feat(scrapy): add year and title selector

--- movies/movies/spiders/cinechile.py
+++ movies/movies/spiders/cinechile.py
@@ -10,7 +10,13 @@ class CinechileSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-    	filename = response.url.split("/")[-2] + '.html'
-    	with open(filename, 'wb') as f:
-			f.write(response.body)
+   #  	filename = response.url.split("/")[-2] + '.html'
+   #  	with open(filename, 'wb') as f:
+			# f.write(response.body)
+		for sel in response.xpath('//table[@id="example2"]/tbody/tr'):
+			title = sel.xpath('td/a/text()').extract()
+			year = sel.xpath('td[2]/text()').extract()
+			#link = sel.xpath('a/@href').extract()
+			#desc = sel.xpath('text()').extract()
+			print title, year
         #pass
